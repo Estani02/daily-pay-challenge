4c970e8b33282838a35c38c4ce55802722849070
Estani02 <estaniolmedo@gmail.com>
2023-06-28 13:28:32 -0300

    added in page component client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import api from '../api'
+import { BallotClientPage } from './client'
 
-export default function Home() {
+export default async function Home() {
+  const ballots = await api.ballot.list()
+
   return (
-    <main className="flex">
-
+    <main className="flex flex-col">
+      <h1 className="text-4xl font-bold mt-5">Movie Awards 2021</h1>
+      <BallotClientPage ballots={ballots} />
     </main>
   )
 }
